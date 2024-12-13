b96298c47026790d8c0514c208030043099d6990
Zvezdo4et10 <vasyakhromov2010@gmail.com>
2024-12-13 23:57:43 +0300

    delete render

--- app.py
+++ app.py
@@ -13,9 +13,3 @@ if st.button("Очистить HTML"):
         cleaned_html = clean_html(html_input)
         st.subheader("Очищенный HTML код:")
         st.code(cleaned_html, language='html')
-        st.subheader("Рендер HTML")
-        with st.container() as container1:
-            container1.markdown("<div class='container1'>", unsafe_allow_html=True)
-            st.markdown(cleaned_html, unsafe_allow_html=True)
-    else:
-        st.warning("Пожалуйста, введите HTML код.")
